6dafa2bbbe2e685fa07f35be766d6776fb6ff8f5
Caleb <me_at_home5@hotmail.com>
2014-01-17 07:25:13 +0000

    updated LocalClassLoader to support jar files

--- src/edu/revtek/util/LocalClassLoader.java
+++ src/edu/revtek/util/LocalClassLoader.java
@@ -1,11 +1,15 @@
 package edu.revtek.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 /**
  * @author Caleb Bradford
@@ -20,20 +24,41 @@ public class LocalClassLoader {
         if (!s.endsWith("/"))
             s += "/";
         URL res = LocalClassLoader.class.getResource(s);
-        File dir = new File(res.toURI());
-        File[] files = dir.listFiles();
-        if (files != null) {
-            for (File f : files) {
-                if (f.isFile()) {
-                    if (f.getName().endsWith(".class")) {
-                        classes.add(Class.forName(_package + '.' + f.getName().split(".class")[0]));
+        if (res.getFile().contains("jar!/")) {
+            getClassesInJar(classes, res, s);
+        } else {
+            File dir = new File(res.toURI());
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    if (f.isFile()) {
+                        if (f.getName().endsWith(".class")) {
+                            classes.add(Class.forName(_package + '.' + f.getName().split(".class")[0]));
+                        }
+                    } else if (recursive) {
+                        Collections.addAll(classes, getClassesIn(_package + '.' + f.getName(), true));
                     }
-                } else if (recursive) {
-                    Collections.addAll(classes, getClassesIn(_package + '.' + f.getName(), true));
                 }
             }
         }
         return classes.toArray(new Class[classes.size()]);
+    }
+
+    private static void getClassesInJar(List<Class> classes, URL res, String s) {
+        String path = res.getPath();
+        String jPath = path.substring(5, path.indexOf("!/"));
+        try {
+            JarInputStream in = new JarInputStream(new FileInputStream(jPath));
+            JarEntry entry;
+            while ((entry = in.getNextJarEntry()) != null) {
+                if (entry.getName().startsWith(s.substring(1)) && entry.getName().endsWith(".class")) {
+                    String className = (entry.getName().split(".class")[0]).replace('/', '.');
+                    classes.add(Class.forName(className));
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
