4dca16fe717dd822d0fee876fa5e557e4bc50be1
Caleb <me_at_home5@hotmail.com>
2014-01-17 08:55:10 +0000

    Client container, added skill data

--- src/edu/revtek/updater/container/ClientContainer.java
+++ src/edu/revtek/updater/container/ClientContainer.java
@@ -1,7 +1,15 @@
 package edu.revtek.updater.container;
 
 import edu.revtek.updater.AbstractContainer;
+import edu.revtek.updater.Updater;
+import edu.revtek.util.asm.InstructionPattern;
+import edu.revtek.util.asm.instructions.AbstractInstruction;
+import edu.revtek.util.asm.instructions.FieldInstruction;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.tree.AbstractInsnNode;
 import jdk.internal.org.objectweb.asm.tree.ClassNode;
+import jdk.internal.org.objectweb.asm.tree.FieldInsnNode;
+import jdk.internal.org.objectweb.asm.tree.MethodNode;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,7 +21,9 @@ public class ClientContainer extends AbstractContainer {
 
     @Override
     public void initKeys(List<String> keys) {
-
+        keys.add("level-array");
+        keys.add("real-level-array");
+        keys.add("exp-array");
     }
 
     @Override
@@ -28,7 +38,31 @@ public class ClientContainer extends AbstractContainer {
 
     @Override
     public void visit(ClassNode cn) {
+        getSkillData();
+    }
 
+    private void getSkillData() {
+        InstructionPattern pattern = new InstructionPattern(20,
+                new AbstractInstruction(Opcodes.ICONST_0),
+                new AbstractInstruction(Opcodes.IRETURN),
+                new AbstractInstruction(Opcodes.ICONST_1),
+                new FieldInstruction(Opcodes.GETSTATIC, null, null, "[I"),
+                new AbstractInstruction(Opcodes.ICONST_2),
+                new FieldInstruction(Opcodes.GETSTATIC, null, null, "[I"),
+                new AbstractInstruction(Opcodes.ICONST_3),
+                new FieldInstruction(Opcodes.GETSTATIC, null, null, "[I")
+
+        );
+        for (ClassNode cn : Updater.get().getClassNodes().values()) {
+            for (MethodNode mn : cn.methods) {
+                AbstractInsnNode[] nodes = pattern.find(mn);
+                if (nodes == null)
+                    continue;
+                hooks.get("level-array").set((FieldInsnNode) nodes[3]);
+                hooks.get("real-level-array").set((FieldInsnNode) nodes[5]);
+                hooks.get("exp-array").set((FieldInsnNode) nodes[7]);
+            }
+        }
     }
 
 }
